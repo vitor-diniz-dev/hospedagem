ab0855be04f578adfa4e7bf38f72333aedd347bb
Vitor Diniz <vitor.diniz.dev@gmail.com>
2025-07-03 19:41:01 -0300

    Configuração de estilo base

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
-  <router-view />
+  <div class="main-container">
+    <router-view />
+  </div>
 </template>
 
 <script setup lang="ts">
 //
 </script>
+
+<style lang="scss">
+.main-container {
+  max-width: 1200px;
+  width: 100%;
+}
+</style>
