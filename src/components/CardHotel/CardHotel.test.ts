ba2962604366b4e86b66aa93d12c62d4b68dddea
Vitor Diniz <vitor.diniz.dev@gmail.com>
2025-07-07 21:21:04 -0300

    Correção testes unitários

--- src/components/CardHotel/CardHotel.test.ts
+++ src/components/CardHotel/CardHotel.test.ts
@@ -1,15 +1,42 @@
 import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-vitest';
 import { render, screen } from '@testing-library/vue';
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it } from 'vitest';
 import CardHotel from './CardHotel.vue';
 import userEvent from '@testing-library/user-event';
+import { createApp } from 'vue';
+import { createPinia, setActivePinia } from 'pinia';
+
+const app = createApp({});
+beforeEach(() => {
+  const pinia = createPinia();
+  app.use(pinia);
+  setActivePinia(pinia);
+});
 
 installQuasarPlugin();
 
 describe('CardHotel', () => {
   it("ao clicar no botao 'ver detalhes, apresentar modal", async () => {
+    // Escuta o método de busca de cidades
     const user = userEvent.setup();
-    render(CardHotel);
+    render(CardHotel, {
+      props: {
+        hotel: {
+          id: 1,
+          name: 'Hotel Copacabana Palace',
+          stars: 5,
+          totalPrice: 580,
+          dailyPrice: 290,
+          tax: 80,
+          thumb: 'https://cdn.quasar.dev/img/parallax2.jpg',
+          amenities: [1, 2, 4, 6],
+          hasBreakFast: true,
+          hasRefundableRoom: true,
+          district: 'Copacabana',
+          placeId: 1,
+        },
+      },
+    });
 
     const botaoVerDetalhes = screen.getByRole('button', { name: /ver detalhes/i });
 
--- src/components/PainelBusca/PainelBusca.test.ts
+++ src/components/PainelBusca/PainelBusca.test.ts
@@ -1,15 +1,26 @@
-import { describe, expect, it, afterEach, vi } from 'vitest';
+/* eslint-disable @typescript-eslint/unbound-method */
+import { describe, expect, it, afterEach, vi, beforeEach } from 'vitest';
 import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-vitest';
-import { cleanup, render, screen } from '@testing-library/vue';
+import { cleanup, render, screen, waitFor } from '@testing-library/vue';
 import userEvent from '@testing-library/user-event';
 import PainelBusca from './PainelBusca.vue';
-import * as cidadeService from '../../services/cidade-service';
-import * as hoteisService from '../../services/hoteis-service';
+import { createPinia, setActivePinia } from 'pinia';
+import { createApp } from 'vue';
+import axios from 'axios';
 
-installQuasarPlugin();
+const app = createApp({});
+beforeEach(() => {
+  const pinia = createPinia();
+  app.use(pinia);
+  setActivePinia(pinia);
+});
 
 // Limpa a renderização após cada teste para evitar os métodos de busca por elemento de encontrarem mais de uma instância do mesmo
 afterEach(() => cleanup());
+
+installQuasarPlugin();
+
+const debounce = () => new Promise((resolve) => setTimeout(resolve, 600));
 
 describe('PainelBusca', () => {
   it('deve conter a aba hotel', () => {
@@ -36,7 +47,11 @@ describe('PainelBusca', () => {
 
   it('ao digitar o terceiro caracter no campo destino, deve buscar cidades', async () => {
     // Escuta o método de busca de cidades
-    const spy = vi.spyOn(cidadeService, 'buscarCidades');
+    const spy = vi.spyOn(axios, 'get').mockResolvedValue({
+      name: 'Rio de Janeiro',
+      state: 'RJ',
+      placeId: 1,
+    });
     const user = userEvent.setup();
 
     render(PainelBusca);
@@ -51,16 +66,33 @@ describe('PainelBusca', () => {
     await user.clear(inputDestino);
     await user.type(inputDestino, 'Rio');
 
+    // waitFor compensa o tempo de debounce do campo de seleção de destino
+    await waitFor(() => {
+      expect(axios.get).toHaveBeenCalledOnce();
+      expect(axios.get).toHaveBeenCalledWith(expect.stringContaining('/cities?name_like=Rio'));
+    });
+
     // Deve buscar cidades com mais de 3 caracteres
     await user.clear(inputDestino);
     await user.type(inputDestino, 'Belo');
 
-    expect(spy).toHaveBeenCalledOnce();
+    await waitFor(() => {
+      expect(axios.get).toHaveBeenCalledTimes(2);
+      expect(axios.get).toHaveBeenCalledWith(expect.stringContaining('/cities?name_like=Belo'));
+    });
   });
 
   it('clicar em pesquisar deve buscar hoteis', async () => {
     // Escuta o método de busca de hotéis
-    const spy = vi.spyOn(hoteisService, 'obterHoteis');
+    const spy = vi.spyOn(axios, 'get').mockResolvedValue({
+      data: [
+        {
+          name: 'Rio de Janeiro',
+          state: 'RJ',
+          placeId: 1,
+        },
+      ],
+    });
     const user = userEvent.setup();
 
     render(PainelBusca);
@@ -68,10 +100,26 @@ describe('PainelBusca', () => {
     const inputDestino = screen.getByPlaceholderText(/destino/i);
     const botaoBuscar = screen.getByRole('button', { name: /buscar hotel/i });
 
+    // Digita o destino a pesquisar
     await user.type(inputDestino, 'Rio');
+    // Aguarda o debounce
+    await debounce();
 
+    // Seleciona uma das cidades
+    const cidade = screen.getByRole('option', { name: /rio de janeiro/i });
+    await user.click(cidade);
+
+    // Clica em buscar
     await user.click(botaoBuscar);
 
-    expect(spy).toHaveBeenCalledOnce();
+    expect(spy).toHaveBeenNthCalledWith(
+      2,
+      expect.stringContaining('/hotels'),
+      expect.objectContaining({
+        params: expect.objectContaining({
+          placeId: 1,
+        }),
+      }),
+    );
   });
 });
